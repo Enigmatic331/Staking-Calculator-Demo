2ade037b6198a07414c7afde7bfc27136d76bb23
Enigmatic331 <unreal_legend7@hotmail.com>
2020-05-12 19:18:26 +0800

    Initial Upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,381 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.groupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txt5Years = New System.Windows.Forms.TextBox()
+        Me.txt4Years = New System.Windows.Forms.TextBox()
+        Me.txt3Years = New System.Windows.Forms.TextBox()
+        Me.txt2Years = New System.Windows.Forms.TextBox()
+        Me.txt1Year = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.textBox2 = New System.Windows.Forms.TextBox()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtUpTime = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtTotalStaked = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtPriceUSD = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtNetworkUptime = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.stakingCost = New System.Windows.Forms.TextBox()
+        Me.txtPrinciple = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.groupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.txtPrinciple)
+        Me.groupBox1.Controls.Add(Me.Label13)
+        Me.groupBox1.Controls.Add(Me.txt5Years)
+        Me.groupBox1.Controls.Add(Me.txt4Years)
+        Me.groupBox1.Controls.Add(Me.txt3Years)
+        Me.groupBox1.Controls.Add(Me.txt2Years)
+        Me.groupBox1.Controls.Add(Me.txt1Year)
+        Me.groupBox1.Controls.Add(Me.Label7)
+        Me.groupBox1.Controls.Add(Me.Label6)
+        Me.groupBox1.Controls.Add(Me.label5)
+        Me.groupBox1.Controls.Add(Me.label4)
+        Me.groupBox1.Controls.Add(Me.label3)
+        Me.groupBox1.Location = New System.Drawing.Point(414, 27)
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.Size = New System.Drawing.Size(281, 382)
+        Me.groupBox1.TabIndex = 8
+        Me.groupBox1.TabStop = False
+        Me.groupBox1.Text = "Results"
+        '
+        'txt5Years
+        '
+        Me.txt5Years.Location = New System.Drawing.Point(132, 175)
+        Me.txt5Years.Name = "txt5Years"
+        Me.txt5Years.Size = New System.Drawing.Size(100, 20)
+        Me.txt5Years.TabIndex = 16
+        Me.txt5Years.Text = "0"
+        '
+        'txt4Years
+        '
+        Me.txt4Years.Location = New System.Drawing.Point(132, 143)
+        Me.txt4Years.Name = "txt4Years"
+        Me.txt4Years.Size = New System.Drawing.Size(100, 20)
+        Me.txt4Years.TabIndex = 15
+        Me.txt4Years.Text = "0"
+        '
+        'txt3Years
+        '
+        Me.txt3Years.Location = New System.Drawing.Point(132, 113)
+        Me.txt3Years.Name = "txt3Years"
+        Me.txt3Years.Size = New System.Drawing.Size(100, 20)
+        Me.txt3Years.TabIndex = 14
+        Me.txt3Years.Text = "0"
+        '
+        'txt2Years
+        '
+        Me.txt2Years.Location = New System.Drawing.Point(132, 83)
+        Me.txt2Years.Name = "txt2Years"
+        Me.txt2Years.Size = New System.Drawing.Size(100, 20)
+        Me.txt2Years.TabIndex = 13
+        Me.txt2Years.Text = "0"
+        '
+        'txt1Year
+        '
+        Me.txt1Year.Location = New System.Drawing.Point(132, 57)
+        Me.txt1Year.Name = "txt1Year"
+        Me.txt1Year.Size = New System.Drawing.Size(100, 20)
+        Me.txt1Year.TabIndex = 12
+        Me.txt1Year.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(35, 178)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(92, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Five Years (USD):"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(33, 146)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(93, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Four Years (USD):"
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Location = New System.Drawing.Point(26, 116)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(100, 13)
+        Me.label5.TabIndex = 4
+        Me.label5.Text = "Three Years (USD):"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Location = New System.Drawing.Point(34, 86)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(93, 13)
+        Me.label4.TabIndex = 3
+        Me.label4.Text = "Two Years (USD):"
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Location = New System.Drawing.Point(39, 60)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(87, 13)
+        Me.label3.TabIndex = 2
+        Me.label3.Text = "One Year (USD):"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(58, 70)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(108, 13)
+        Me.label2.TabIndex = 7
+        Me.label2.Text = "Number of Validators:"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(68, 44)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(98, 13)
+        Me.label1.TabIndex = 6
+        Me.label1.Text = "Total ETH Deposit:"
+        '
+        'textBox2
+        '
+        Me.textBox2.Location = New System.Drawing.Point(172, 67)
+        Me.textBox2.Name = "textBox2"
+        Me.textBox2.Size = New System.Drawing.Size(100, 20)
+        Me.textBox2.TabIndex = 11
+        Me.textBox2.Text = "1"
+        '
+        'textBox1
+        '
+        Me.textBox1.Enabled = False
+        Me.textBox1.Location = New System.Drawing.Point(172, 41)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(100, 20)
+        Me.textBox1.TabIndex = 10
+        Me.textBox1.Text = "32"
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(620, 419)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(75, 23)
+        Me.button1.TabIndex = 9
+        Me.button1.Text = "Calculate"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(30, 112)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(136, 13)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Estimated Validator Uptime:"
+        '
+        'txtUpTime
+        '
+        Me.txtUpTime.Location = New System.Drawing.Point(172, 109)
+        Me.txtUpTime.Name = "txtUpTime"
+        Me.txtUpTime.Size = New System.Drawing.Size(100, 20)
+        Me.txtUpTime.TabIndex = 13
+        Me.txtUpTime.Text = "0.99"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(30, 144)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(139, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Total Network ETH Staked:"
+        '
+        'txtTotalStaked
+        '
+        Me.txtTotalStaked.Location = New System.Drawing.Point(172, 141)
+        Me.txtTotalStaked.Name = "txtTotalStaked"
+        Me.txtTotalStaked.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotalStaked.TabIndex = 15
+        Me.txtTotalStaked.Text = "524288"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(75, 194)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(91, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "ETH Price (USD):"
+        '
+        'txtPriceUSD
+        '
+        Me.txtPriceUSD.Location = New System.Drawing.Point(172, 191)
+        Me.txtPriceUSD.Name = "txtPriceUSD"
+        Me.txtPriceUSD.Size = New System.Drawing.Size(100, 20)
+        Me.txtPriceUSD.TabIndex = 17
+        Me.txtPriceUSD.Text = "190.00"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtNetworkUptime)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 278)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(383, 131)
+        Me.GroupBox2.TabIndex = 18
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Other Parameters"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(18, 48)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(106, 13)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "Network Uptime (%): "
+        '
+        'txtNetworkUptime
+        '
+        Me.txtNetworkUptime.Location = New System.Drawing.Point(130, 45)
+        Me.txtNetworkUptime.Name = "txtNetworkUptime"
+        Me.txtNetworkUptime.Size = New System.Drawing.Size(100, 20)
+        Me.txtNetworkUptime.TabIndex = 18
+        Me.txtNetworkUptime.Text = "0.90"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(35, 226)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(134, 13)
+        Me.Label12.TabIndex = 19
+        Me.Label12.Text = "Staking Cost (USD-Yearly):"
+        '
+        'stakingCost
+        '
+        Me.stakingCost.Location = New System.Drawing.Point(172, 223)
+        Me.stakingCost.Name = "stakingCost"
+        Me.stakingCost.Size = New System.Drawing.Size(100, 20)
+        Me.stakingCost.TabIndex = 20
+        Me.stakingCost.Text = "500.00"
+        '
+        'txtPrinciple
+        '
+        Me.txtPrinciple.Location = New System.Drawing.Point(132, 27)
+        Me.txtPrinciple.Name = "txtPrinciple"
+        Me.txtPrinciple.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrinciple.TabIndex = 18
+        Me.txtPrinciple.Text = "0"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(44, 30)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(82, 13)
+        Me.Label13.TabIndex = 17
+        Me.Label13.Text = "Principle (USD):"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(734, 450)
+        Me.Controls.Add(Me.stakingCost)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.txtPriceUSD)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.txtTotalStaked)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtUpTime)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.groupBox1)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.textBox2)
+        Me.Controls.Add(Me.textBox1)
+        Me.Controls.Add(Me.button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.groupBox1.ResumeLayout(False)
+        Me.groupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents groupBox1 As GroupBox
+    Private WithEvents label5 As Label
+    Private WithEvents label4 As Label
+    Private WithEvents label3 As Label
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
+    Private WithEvents textBox2 As TextBox
+    Private WithEvents textBox1 As TextBox
+    Private WithEvents button1 As Button
+    Private WithEvents txt5Years As TextBox
+    Private WithEvents txt4Years As TextBox
+    Private WithEvents txt3Years As TextBox
+    Private WithEvents txt2Years As TextBox
+    Private WithEvents txt1Year As TextBox
+    Private WithEvents Label7 As Label
+    Private WithEvents Label6 As Label
+    Private WithEvents Label8 As Label
+    Private WithEvents txtUpTime As TextBox
+    Private WithEvents Label9 As Label
+    Private WithEvents txtTotalStaked As TextBox
+    Private WithEvents Label10 As Label
+    Private WithEvents txtPriceUSD As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Private WithEvents txtNetworkUptime As TextBox
+    Private WithEvents Label11 As Label
+    Private WithEvents Label12 As Label
+    Private WithEvents stakingCost As TextBox
+    Private WithEvents txtPrinciple As TextBox
+    Private WithEvents Label13 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        ' hardcoded variables first for testing
+
+        'validator and price parameters
+        Dim ETHPrice As Decimal = txtPriceUSD.Text
+        Dim perValidatorDeposit = 32
+        Dim totalNumberOfETH = textBox1.Text
+        Dim numberOfValidators = textBox2.Text
+        Dim totalStaked As Decimal = txtTotalStaked.Text
+        Dim avgNetworkOnline = txtNetworkUptime.Text
+        Dim validatorUpTime = txtUpTime.Text
+
+        'network parameters
+        Dim shardCount = 64
+        Dim slotTime = 12 'seconds
+        Dim SlotsPerEpoch = 32
+        Dim epochsPerYear = (60 * 60 * 24 * 365.242) / (slotTime * SlotsPerEpoch) 'adjusted for leap years
+
+        'reward and penalty parameters
+        Dim baseRewardFactor = 64 'Base Reward Factor
+        Dim baseRewardsPerEpoch = 4
+        Dim baseRewardProportionalValidatorsOnline = 3
+        Dim inclusionPenaltyForOneSlotLate = 1 / 64
+
+        'calculate base reward for full validator
+        Dim baseRewardForFullValidator = 32 * (10 ^ 9) * baseRewardFactor / Math.Sqrt(totalStaked * (10 ^ 9)) / baseRewardsPerEpoch
+
+        'calculate online per-validator reward per epoch
+        Dim onlinePerValidatorRewardPerEpoch = baseRewardForFullValidator * baseRewardProportionalValidatorsOnline * avgNetworkOnline _
+                                                + 1 * (0.125 * baseRewardForFullValidator * avgNetworkOnline + 0.875 * baseRewardForFullValidator *
+                                                (avgNetworkOnline + avgNetworkOnline * (1 - avgNetworkOnline) * (1 - inclusionPenaltyForOneSlotLate) +
+                                                avgNetworkOnline * (1 - avgNetworkOnline) ^ 2 * (1 - 2 * inclusionPenaltyForOneSlotLate)))
+
+        'calculate offline per-validator penalty per epoch
+        Dim offlinePerValidatorPenaltyPerEpoch = baseRewardForFullValidator * 4
+        Dim stakingInterest = (onlinePerValidatorRewardPerEpoch * validatorUpTime - offlinePerValidatorPenaltyPerEpoch * (1 - validatorUpTime)) * epochsPerYear _
+                                / 10 ^ 9 / perValidatorDeposit
+
+
+        'tabulate for 5 years
+        txtPrinciple.Text = ETHPrice * totalNumberOfETH
+        txt1Year.Text = Math.Round(totalNumberOfETH * (1 + stakingInterest) * ETHPrice, 2) - stakingCost.Text
+        txt2Years.Text = Math.Round(txt1Year.Text * (1 + stakingInterest), 2) - stakingCost.Text
+        txt3Years.Text = Math.Round(txt2Years.Text * (1 + stakingInterest), 2) - stakingCost.Text
+        txt4Years.Text = Math.Round(txt3Years.Text * (1 + stakingInterest), 2) - stakingCost.Text
+        txt5Years.Text = Math.Round(txt4Years.Text * (1 + stakingInterest), 2) - stakingCost.Text
+
+    End Sub
+
+    Private Sub textBox2_TextChanged(sender As Object, e As EventArgs) Handles textBox2.TextChanged
+        If IsNumeric(textBox2.Text) Then
+            textBox1.Text = textBox2.Text * 32
+        End If
+    End Sub
+End Class
